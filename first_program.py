f2935aedca00ac7ac0ed0529562f59b2f1455bf9
nikeshgamal24 <nikesh.gamal24@gmail.com>
2023-01-05 20:09:03 +0545

    reverse a string

--- first_program.py
+++ first_program.py
@@ -1,1 +1,24 @@
-print('Hello World')
+#program to reverse a string using function
+
+def reverse_string(local_string: str)-> str:
+    result_string = ''
+    for ind in range(len(local_string)-1,-1,-1):
+    
+        # print(f'index: {ind} value: {local_string[ind]}')
+        # print(f'old-result: {result_string}')
+
+        result_string = result_string +  local_string[ind]
+
+        # print(f'new-result:{result_string}')
+    
+    return result_string
+
+input_string = input("Enter the desirable string: ")
+
+# #ouput--> hahsinim
+
+result = reverse_string(input_string)
+print('Outside of function')
+print(input_string)
+print('The reverse of the string is:')
+print(result)
